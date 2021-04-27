37b543997b6763315336ece42183c061b8a093e8
Park YuYeol <qkrdbduf1025@naver.com>
2021-04-27 11:12:29 +0900

    Initial Commit

---
+++ Mymemory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mymemory
+//
+//  Created by Joung Kisang on 2021/04/27.
+//  Copyright © 2021 rubypaper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
